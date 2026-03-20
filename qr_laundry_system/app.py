ed0341fee07db63316c5f20a3e22c8becde044b3
CHARAN REDDY POLU <polucharanreddy@gmail.com>
2026-03-20 13:13:21 +0530

    Update app.py

--- qr_laundry_system/app.py
+++ qr_laundry_system/app.py
@@ -71,7 +71,7 @@ def register():
 # ---------------- LOGIN ----------------
 @app.route("/")
 def home():
-    return "Working Home Page ✅"
+    return redirect(url_for("login"))
 @app.route("/login", methods=["GET","POST"])
 def login():
     if request.method == "POST":
